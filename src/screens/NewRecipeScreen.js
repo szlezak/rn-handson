006ea9e244ea91293c4b09d4ac123628eef670af
szlezak <robertszlezak@gmail.com>
2017-08-07 21:57:18 +0200

    Added forms

--- src/screens/NewRecipeScreen.js
+++ src/screens/NewRecipeScreen.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Text, TouchableHighlight, View } from 'react-native';
+import t from 'tcomb-form-native';
+
+const Form = t.form.Form;
+
+const Recipe = t.struct({
+  title: t.String,
+  title: t.String,
+  image: t.String,
+  preparationTime: t.Number,
+  servingCount: t.Number,
+  directions: t.String,
+});
 
 class NewRecipeScreen extends Component {
+  _onPress = () => {
+    let value = this.refs.recipeForm.getValue();
+    if (value) {
+      console.log(value);
+    }
+  }
+
   render() {
     return (
       <View>
-        <Text>NewRecipeScreen</Text>
+        <Form
+          ref="recipeForm"
+          type={Recipe}
+        />
+        <TouchableHighlight onPress={this._onPress}>
+          <Text>Save</Text>
+        </TouchableHighlight>
       </View>
     );
   }
