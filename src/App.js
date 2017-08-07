db670ac652d446d61eb2b7af107ca72238d92c21
szlezak <robertszlezak@gmail.com>
2017-08-07 19:30:21 +0200

    Removed mock data from app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import { CookbookTab } from './navigation/router';
 
-import { recipeListData } from './helper/mock-data';
-
 class App extends Component {
   render() {
     return <CookbookTab />;
